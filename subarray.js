906a46f6ed9d4bf43a0fb8e9da1d35a728af5e6e
Frank W <manager@1touchsystems.com>
2019-04-16 18:35:57 +0000

    Final working subarray

--- subarray.js
+++
@@ -1,7 +0,0 @@
-// Find the length of longest continuous subarray within an array (containing at least one number) whose sum equals zero.
-
-// For example, given the array [1,0,-1,2],
-
-// The longest continuous subarray of zero-sum is [1,0,-1] which has length = 3
-let arr = Array.from({length: 40}, () => Math.floor(Math.random() * 40));
-debugger
