9c68daae900a8c3db79c8dd97ba2d7b1620bfdd9
Frank W <manager@1touchsystems.com>
2019-04-26 15:38:44 +0000

    Swap2

--- swap-linked-list.js
+++ swap-linked-list.js
@@ -36,5 +36,34 @@ headS.next.next.next.next = new ListNode(5);
 headS.next.next.next.next.next = new ListNode(6);
 headS.next.next.next.next.next.next = new ListNode(7);
 headS.next.next.next.next.next.next.next = new ListNode(8);
-let result = swapPairs(headS);
-console.log(result);
+// let result = swapPairs(headS);
+// console.log(result);
+
+// Input: 1->2->3->4->5->NULL
+// Output: 5->4->3->2->1->NULL
+// A linked list can be reversed either iteratively or recursively. Could you implement both?
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    if (!head){
+        return null;
+    }
+    if (!head.next){
+        return head;
+    }
+    const nextNode = reverseList(head.next);
+    const p2 = head.next;
+    p2.next = head;
+    head.next = node;
+    return p2;
+};
