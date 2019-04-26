1375959335123fe28a2b297c38a40f83e827cb94
Frank W <manager@1touchsystems.com>
2019-04-26 15:24:06 +0000

    pascal

---
+++ pascal.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+var generate = function(numRows) {
+    if (numRows) {
+        var result = [[1]];
+
+        for(var i = 1;i < numRows;i++) {
+            result[i] = [];
+            for(var j = 0; j < i + 1;j++) {
+                result[i][j] = (result[i - 1][j] || 0) + (result[i - 1][j - 1] || 0);
+            }
+        }
+            
+        return result;
+    }else {
+        return [];
+    }
+};
+let nP = generate(8);
+console.log(nP);
