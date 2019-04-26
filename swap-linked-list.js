7930fd41155a6c26e355f353f7904271d98e75d4
Frank W <manager@1touchsystems.com>
2019-04-26 14:01:21 +0000

    Final Version swap

--- swap-linked-list.js
+++ swap-linked-list.js
@@ -13,19 +13,45 @@ function ListNode(data){
     this.val = data;
     this.next = null;
 }
-var swapPairs = function(head) {
-    var cNode = head;
 
-    while(cNode.next.next){
-        let nextNode = cNode.next.next;
-        let nodeA = cNode.val;
-        nodeA.next = nextNode;
-        let nodeB = cNode.next
-        cNode.val = nodeB;
-        cNode.next = nodeA
-        cNode = nextNode;
+var swapPairs = function(head) {
+    const swap = (head) =>{
+        if(!head.next){
+            return head;
+        }else {
+            let nA = head.val;
+            let nB = head.next.val;
+            head.val = nB;
+            head.next.val = nA;
+    
+            if(head.next.next){
+                head.next.next = swap(head.next.next);
+                return head
+                
+            }else{
+                return head;
+            }
+        }
     }
-    return head
+    let A = null;
+    let B = null;
+    if(!head.next){
+        return head;
+    }else {
+        A = head.val;
+        B = head.next.val;
+        head.next.val = A;
+        head.val = B;
+
+        if (!head.next.next){
+            return head;
+        }else {
+            let subList = swap(head.next.next);
+            head.next.next = subList;
+            return head;
+        }   
+    }
+
 
 };
 
@@ -33,4 +59,9 @@ let headS = new ListNode(1);
 headS.next = new ListNode(2);
 headS.next.next = new ListNode(3);
 headS.next.next.next = new ListNode(4);
-swapPairs(headS);
+headS.next.next.next.next = new ListNode(5);
+headS.next.next.next.next.next = new ListNode(6);
+headS.next.next.next.next.next.next = new ListNode(7);
+headS.next.next.next.next.next.next.next = new ListNode(8);
+let result = swapPairs(headS);
+console.log(result);
